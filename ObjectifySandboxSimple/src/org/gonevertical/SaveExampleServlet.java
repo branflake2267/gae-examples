587421782967ee2412a45027985666c29bd5aa9e
Brandon Donnelson <branflake2267@gmail.com>
2012-06-28 23:10:26 -0700

    Adding more simple examples

--- ObjectifySandboxSimple/src/org/gonevertical/SaveExampleServlet.java
+++ ObjectifySandboxSimple/src/org/gonevertical/SaveExampleServlet.java
@@ -13,19 +13,14 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 
 @SuppressWarnings("serial")
-public class HomeServlet extends HttpServlet {
+public class SaveExampleServlet extends HttpServlet {
   
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("HomeServlet Works");
     
     Objectify ofy = OfyFactory.init();
     
     Person person = new Person("Brandon");
-    ofy.save().entities(person).now();
-    resp.getWriter().println("person=" + person);
-    
-    person.setName("Brandon2");
     ofy.save().entities(person).now();
     resp.getWriter().println("person=" + person);
     
