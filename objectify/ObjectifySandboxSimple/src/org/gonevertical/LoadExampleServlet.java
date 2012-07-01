a96c9807035fdfc7b871f78f64565aaad6a2491e
Brandon Donnelson <branflake2267@gmail.com>
2012-06-30 21:53:03 -0700

    Loading Examples

--- objectify/ObjectifySandboxSimple/src/org/gonevertical/LoadExampleServlet.java
+++ objectify/ObjectifySandboxSimple/src/org/gonevertical/LoadExampleServlet.java
@@ -1,6 +1,7 @@
 package org.gonevertical;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,35 +14,56 @@ import com.googlecode.objectify.Objectify;
 
 @SuppressWarnings("serial")
 public class LoadExampleServlet extends HttpServlet {
-  
+
   private HttpServletRequest request;
   private HttpServletResponse response;
-  
+
   /**
    * Objectify Factory which registers the service and classes we need like Person.class
    */
   private Objectify ofy = OfyFactory.init();
-  
+
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     this.request = request;
     this.response = response;
-    
+
     response.setContentType("text/plain");
-    
+
     example1();
+    example2();
+    example3();
   }
-  
+
   /**
    * Load
    */
-  private void example1() throws IOException  {
+  private void example1() throws IOException {
     Person person = new Person("Brandon");
     ofy.save().entities(person).now();
     response.getWriter().println("person=" + person);
-    
+
     long id = person.getId();
     person = ofy.load().type(Person.class).id(id).get();
     response.getWriter().println("person=" + person);
+  }
+
+  /**
+   * Load Count
+   */
+  private void example2() throws IOException {
+    int count = ofy.load().type(Person.class).count();
+    response.getWriter().println("person count=" + count);
+  }
+
+  /**
+   * Load all with Filter by Name
+   */
+  private void example3() throws IOException {
+    String findName = "Brandon";
+    List<Person> list = ofy.load().type(Person.class).filter("name", findName).list();
+    for (Person person : list) {
+      response.getWriter().println("item: person=" + person);
+    }
   }
   
 }
--- objectify/ObjectifySandboxSimple/src/org/gonevertical/entities/Person.java
+++ objectify/ObjectifySandboxSimple/src/org/gonevertical/entities/Person.java
@@ -2,6 +2,7 @@ package org.gonevertical.entities;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 
 @Entity
 public class Person {
@@ -9,8 +10,11 @@ public class Person {
   @Id
   private Long id;
   
+  @Index
   private String name;
   
+  private int age;
+
   public Person() { 
   }
 
@@ -21,7 +25,6 @@ public class Person {
   public Long getId() {
     return id;
   }
-
   public void setId(Long id) {
     this.id = id;
   }
@@ -29,15 +32,22 @@ public class Person {
   public String getName() {
     return name;
   }
-
   public void setName(String name) {
     this.name = name;
+  }
+  
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
   }
   
   public String toString() {
     String s = "";
     s += "id=" + getId() + " ";
     s += "name=" + name + " ";
+    s += "age=" + age + " ";
     return s;
   }
   
