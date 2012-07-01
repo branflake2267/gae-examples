69d5a41fcbf7bb1b91445d75a91159ec48b4621f
Brandon Donnelson <branflake2267@gmail.com>
2012-06-30 19:38:13 -0700

    More Objectify examples

--- objectify/ObjectifySandboxSimple/src/org/gonevertical/SaveExampleServlet.java
+++ objectify/ObjectifySandboxSimple/src/org/gonevertical/SaveExampleServlet.java
@@ -1,6 +1,8 @@
 package org.gonevertical;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,18 +17,61 @@ import com.googlecode.objectify.ObjectifyService;
 @SuppressWarnings("serial")
 public class SaveExampleServlet extends HttpServlet {
   
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setContentType("text/plain");
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  
+  /**
+   * Objectify Factory which registers the service and classes we need like Person.class
+   */
+  private Objectify ofy = OfyFactory.init();
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    this.request = request;
+    this.response = response;
     
-    Objectify ofy = OfyFactory.init();
+    response.setContentType("text/plain");
     
+    example1();
+    example2();
+    example3();
+  }
+  
+  /**
+   * Save
+   */
+  private void example1() throws IOException {
     Person person = new Person("Brandon");
     ofy.save().entities(person).now();
-    resp.getWriter().println("person=" + person);
+    response.getWriter().println("person=" + person);
     
+    // Load
     long id = person.getId();
     person = ofy.load().type(Person.class).id(id).get();
-    resp.getWriter().println("person=" + person);
+    response.getWriter().println("person=" + person);
+  }
+  
+  /**
+   * Save an Array
+   */
+  private void example2() throws IOException {
+    Person[] people = new Person[2];
+    people[0] = new Person("Fred");
+    people[1] = new Person("Barney");
+    ofy.save().entities(people).now();
+    response.getWriter().println("people[0]=" + people[0]);
+    response.getWriter().println("people[1]=" + people[1]);
+  }
+  
+  /**
+   * Save a List
+   */
+  private void example3() throws IOException { 
+    List<Person> people = new ArrayList<Person>();
+    people.add(new Person("Fred"));
+    people.add(new Person("Barney"));
+    ofy.save().entities(people).now();
+    response.getWriter().println("people.get(0)=" + people.get(0));
+    response.getWriter().println("people.get(1)=" + people.get(1));
   }
   
 }
